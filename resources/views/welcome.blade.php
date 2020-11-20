f3a6338d96c609f39818aeec61045ecc42348a0a
unknown <leereal08@ymail.com>
2020-11-20 18:26:51 +0200

    Favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
      <meta name="twitter:image" content="https:e-coince.com">
 
     <!-- Favicons -->
-    <link href="{{asset('favicon.png')}}" rel="icon">
+    <link href="{{asset('assets/favicon.png')}}" rel="icon">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
