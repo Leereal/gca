3b71cd654f0a8c1c5da74edfb264b0fa2d66b7a6
unknown <leereal08@ymail.com>
2020-11-21 07:50:19 +0200

    IP Address added on Auction seeder

--- database/seeders/AuctionSeeder.php
+++ database/seeders/AuctionSeeder.php
@@ -14,15 +14,15 @@ class AuctionSeeder extends Seeder
      */
     public function run()
     {
-        Auction::create(['amount'=>'10000','balance'=>'1200','bank_detail_id'=>'1','investment_id'=>'1','user_id'=>'1']);
-        Auction::create(['amount'=>'10000','balance'=>'250','bank_detail_id'=>'1','investment_id'=>'1','user_id'=>'1']);
-        Auction::create(['amount'=>'10000','balance'=>'310','bank_detail_id'=>'4','investment_id'=>'2','user_id'=>'4']);
-        Auction::create(['amount'=>'10000','balance'=>'500','bank_detail_id'=>'4','investment_id'=>'2','user_id'=>'4']);
-        Auction::create(['amount'=>'10000','balance'=>'2100','bank_detail_id'=>'2','investment_id'=>'3','user_id'=>'3']);
-        Auction::create(['amount'=>'10000','balance'=>'260','bank_detail_id'=>'2','investment_id'=>'3','user_id'=>'3']);
-        Auction::create(['amount'=>'10000','balance'=>'290','bank_detail_id'=>'5','investment_id'=>'4','user_id'=>'6']); 
-        Auction::create(['amount'=>'10000','balance'=>'250','bank_detail_id'=>'5','investment_id'=>'4','user_id'=>'6']);
-        Auction::create(['amount'=>'10000','balance'=>'250','bank_detail_id'=>'6','investment_id'=>'1','user_id'=>'1']);
-        Auction::create(['amount'=>'10000','balance'=>'1000','bank_detail_id'=>'6','investment_id'=>'1','user_id'=>'1']);           
+        Auction::create(['amount'=>'10000','balance'=>'1200','bank_detail_id'=>'1','investment_id'=>'1','user_id'=>'1','ipaddress'=>'127.0.0.1']);
+        Auction::create(['amount'=>'10000','balance'=>'250','bank_detail_id'=>'1','investment_id'=>'1','user_id'=>'1','ipaddress'=>'127.0.0.1']);
+        Auction::create(['amount'=>'10000','balance'=>'310','bank_detail_id'=>'4','investment_id'=>'2','user_id'=>'4','ipaddress'=>'127.0.0.1']);
+        Auction::create(['amount'=>'10000','balance'=>'500','bank_detail_id'=>'4','investment_id'=>'2','user_id'=>'4','ipaddress'=>'127.0.0.1']);
+        Auction::create(['amount'=>'10000','balance'=>'2100','bank_detail_id'=>'2','investment_id'=>'3','user_id'=>'3','ipaddress'=>'127.0.0.1']);
+        Auction::create(['amount'=>'10000','balance'=>'260','bank_detail_id'=>'2','investment_id'=>'3','user_id'=>'3','ipaddress'=>'127.0.0.1']);
+        Auction::create(['amount'=>'10000','balance'=>'290','bank_detail_id'=>'5','investment_id'=>'4','user_id'=>'6','ipaddress'=>'127.0.0.1']); 
+        Auction::create(['amount'=>'10000','balance'=>'250','bank_detail_id'=>'5','investment_id'=>'4','user_id'=>'6','ipaddress'=>'127.0.0.1']);
+        Auction::create(['amount'=>'10000','balance'=>'250','bank_detail_id'=>'6','investment_id'=>'1','user_id'=>'1','ipaddress'=>'127.0.0.1']);
+        Auction::create(['amount'=>'10000','balance'=>'1000','bank_detail_id'=>'6','investment_id'=>'1','user_id'=>'1','ipaddress'=>'127.0.0.1']);           
     }
 }
