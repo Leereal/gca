6fa4a0dc8b0b3c9ed14f8c96f0cd024606fdddfd
Leereal <leereal08@ymail.com>
2020-12-19 15:12:04 +0200

    Add Botswana banks

--- database/seeders/BankSeeder.php
+++ database/seeders/BankSeeder.php
@@ -17,45 +17,10 @@ class BankSeeder extends Seeder
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         // Truncate the table.
-        DB::table('banks')->truncate();
-        Bank::create(['name'=>'BITCOIN','avatar'=>'bitcoin.png']);
-        // Bank::create(['name'=>'SKRILL','avatar'=>'skrill.png']);
-        // Bank::create(['name'=>'NETELLER','avatar'=>'neteller.png']);
-        // Bank::create(['name'=>'PERFECT MONEY','avatar'=>'perfect_money.png']);
-        Bank::create(['name'=>'EWALLET/CELLPHONE BANKING','avatar'=>'mobile_banking.png']);
-        Bank::create(['name'=>'FNB (SOUTH AFRICA)','avatar'=>'fnb.png']);
-        Bank::create(['name'=>'STANDARD BANK (SOUTH AFRICA)','avatar'=>'standard_bank.png']);
-        Bank::create(['name'=>'NEDBANK (SOUTH AFRICA)','avatar'=>'nedbank.png']);
-        Bank::create(['name'=>'CAPITEC (SOUTH AFRICA)','avatar'=>'capitec.png']);
-        Bank::create(['name'=>'ABSA (SOUTH AFRICA)','avatar'=>'absa.png']);
-        Bank::create(['name'=>'AFRICAN BANK  (SOUTH AFRICA)','avatar'=>'africa_bank.png']);
-        Bank::create(['name'=>'BIDVEST (SOUTH AFRICA)','avatar'=>'bidvest.png']);
-        Bank::create(['name'=>'DISCOVERY (SOUTH AFRICA)','avatar'=>'discovery.png']);
-        Bank::create(['name'=>'GRINDROD (SOUTH AFRICA)','avatar'=>'grindrod.png']);
-        Bank::create(['name'=>'GROBANK (SOUTH AFRICA)','avatar'=>'grobank.png']);
-        Bank::create(['name'=>'IMPERIAL (SOUTH AFRICA)','avatar'=>'imperial.png']);
-        Bank::create(['name'=>'SASFIN (SOUTH AFRICA)','avatar'=>'sasfin.png']);
-        Bank::create(['name'=>'TYMEBANK (SOUTH AFRICA)','avatar'=>'tymebank.png']);
-        Bank::create(['name'=>'ABSA (BOTSWANA)','avatar'=>'absa.png']);
-        Bank::create(['name'=>'BANCABC (BOTSWANA)','avatar'=>'bancabc.png']);
-        Bank::create(['name'=>'FIRST CAPITAL (BOTSWANA)','avatar'=>'first_capital.png']);
+        DB::table('banks')->truncate();      
+        Bank::create(['name'=>'ORANGE','avatar'=>'orange.png']);
+        Bank::create(['name'=>'MY ZAKA','avatar'=>'myzaka.png']);
         Bank::create(['name'=>'FNB (BOTSWANA)','avatar'=>'fnb.png']);
-        Bank::create(['name'=>'STANBIC (BOTSWANA)','avatar'=>'stanbic.png']);
-        Bank::create(['name'=>'STANDARD (BOTSWANA)','avatar'=>'standard_bank.png']);        
-        Bank::create(['name'=>'NEDBANK (NAMIBIA)','avatar'=>'nedbank.png']);
-        Bank::create(['name'=>'STANDARD (NAMIBIA)','avatar'=>'standard_bank.png']);
-        Bank::create(['name'=>'TRUSTCO (NAMIBIA)','avatar'=>'trustco.png']);
-        Bank::create(['name'=>'ATLANTICO (NAMIBIA)','avatar'=>'atlantico.png']);
-        Bank::create(['name'=>'BANK BIC (NAMIBIA)','avatar'=>'bankbic.png']);
-        Bank::create(['name'=>'LETSHEGO (NAMIBIA)','avatar'=>'letshego.png']);
-        Bank::create(['name'=>'NEDBANK (SWAZILAND)','avatar'=>'nedbank.png']);
-        Bank::create(['name'=>'STANDARD (SWAZILAND)','avatar'=>'standard_bank.png']);
-        Bank::create(['name'=>'FNB (SWAZILAND)','avatar'=>'fnb.png']);
-        Bank::create(['name'=>'STANDARD (LESOTHO)','avatar'=>'standard_bank.png']);
-        Bank::create(['name'=>'NEDBANK (LESOTHO)','avatar'=>'nedbank.png']);
-        Bank::create(['name'=>'FNB (LESOTHO)','avatar'=>'fnb.png']);
-        Bank::create(['name'=>'MPESA (LESOTHO)','avatar'=>'mpesa.png']);
-        Bank::create(['name'=>'FNB (NAMIBIA)','avatar'=>'fnb.png']);
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,11 +24,11 @@ class DatabaseSeeder extends Seeder
             PlanSeeder::class,  
             //AuctionSeeder::class           
         ]); 
-        User::factory(20)->create();
-        BankDetail::factory(20)->create(); 
-        Investment::factory(60)->create();   
-        Auction::factory(20)->create(); 
-        Bids::factory(50)->create();
-        Bonus::factory(50)->create();
+        //User::factory(20)->create();
+        //BankDetail::factory(20)->create(); 
+        //Investment::factory(60)->create();   
+        //Auction::factory(20)->create(); 
+        //Bids::factory(50)->create();
+        //Bonus::factory(50)->create();
     }
 }
